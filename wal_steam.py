e7445778f3e6ba9ad9491906286fd344c8ba19fe
Dakota Walsh <kotawalsh@gmail.com>
2017-08-19 04:57:25 -0400

    prepare to create parser

--- wal_steam.py
+++ wal_steam.py
@@ -26,12 +26,28 @@ Options:
 """
 from lib.docopt import docopt
 
+def parseCss():
+    print("Parsing CSS ")
+
+def getWpg():
+    print("Getting WPG")
+    f_name = open('test.css', 'r')
+    parseCss()
+    f_name.close()
+
+def getWal():
+    print("Getting WAL")
+    f_name = open('test.css', 'r')
+    parseCss()
+    f_name.close()
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Wal Steam 0.1.0') # create the flags from the comment
 
-    if (arguments['--help']==False and arguments['--version']==False):
+    if (arguments['--help']==False and arguments['--version']==False): # determine which option was selected
         if (arguments['-g']==True):
             print("OPTION G WOO!")
+            getWpg()
         else:
-            print("OPTION W WOO!")
+            print("OPTION W WOO!") 
+            getWal()
