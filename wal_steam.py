e7bef97fe1788b829ab7719af5c061b97b7dad33
Dakota Walsh <kotawalsh@gmail.com>
2017-08-19 04:06:12 -0400

    moved library, added help, added version

--- wal_steam.py
+++ wal_steam.py
@@ -10,5 +10,24 @@
 # Copyright (C) 2017  Dakota Walsh #
 #----------------------------------#
 
-print ("Wal for steam")
+"""
+Wal Steam
 
+Usage:
+  
+  wal_steam.py (-w | -g)
+  wal_steam.py (-h | --help)
+  wal_steam.py (-v | --version)
+
+Options:
+  -w                   use wal for colors
+  -g                   use wpg for colors
+  -h --help            show this help message and exit
+  -v --version         show version and exit
+"""
+from lib.docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Wal Steam 0.1.0')
+    print(arguments)
