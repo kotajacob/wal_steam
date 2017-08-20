9aff8618cbfce34a21867efb30470cf3e5877e45
Dakota Walsh <kotawalsh@gmail.com>
2017-08-19 23:13:28 -0400

    add location variables, start new functions

--- wal_steam.py
+++ wal_steam.py
@@ -28,20 +28,37 @@ from lib.docopt import docopt
 import os
 
 # set some variables for the file locations
-wpgConfig = os.path.expanduser("~/.wallpapers/current.css")
-walConfig = os.path.expanduser("~/.cache/wal/colors.css")
+wpgConfig     = os.path.expanduser("~/.wallpapers/current.css")
+walConfig     = os.path.expanduser("~/.cache/wal/colors.css")
+metroSettings = os.path.expanduser("~/.steam/steam/skins/Fake\ Skin/settings.styles") # REPLACE AFTER TESTS
+metroColors   = os.path.expanduser("~/.steam/steam/skins/Fake\ Skin/wal_colors.styles") # REPLACE AFTER TESTS
 
+def makeStyle(colors):
+    # create and write the wal_colors.styles file
+    print("Makeing color styles")
+    print(colors)
+
+def replaceSettings():
+    # replace the settings.styles file with one tweaked to load our colors :)
+    # first make a backup of their settings.styles file
+    print("Replacing settings")
 
 def hexToRgb(hexColors):
     # convert hex colors to rgb colors (takes a list)
+    tmpColors = []
     rgbColors = []
-    for hexC in hexColors: # loop through the hex colors
+    for color in hexColors: # loop through the hex colors
         # remove the optothorpe
         # use tuple and a loop to convert them to rgb
         # append new colors to our rgb list
-        tmp = hexC.lstrip('#')
-        rgbColors.append(tuple(int(tmp[i:i+2], 16) for i in (0, 2 ,4)))
-    print(rgbColors) # debug
+        tmp = color.lstrip('#')
+        tmpColors.append(tuple(int(tmp[i:i+2], 16) for i in (0, 2 ,4)))
+    # put the colors in the correct format
+    for color in tmpColors: # loop through the new RGB colors
+        # remove parentheses
+        tmp = color
+        rgbColors.append(tmp)
+
     return rgbColors
 
 def parseCss(config):
@@ -62,13 +79,17 @@ def parseCss(config):
     return colors
 
 def main(arguments):
-    if (arguments['--help']==False and arguments['--version']==False): # determine which option was selected
-        if (arguments['-g']==True):
+    if (arguments['--help'] == False and arguments['--version'] == False): # determine the mode
+        if (arguments['-g'] == True):
             colors = parseCss(wpgConfig) # they picked g so parse wpg
             colors = hexToRgb(colors)
+            makeStyle(colors)
+            replaceSettings()
         else:
             colors = parseCss(walConfig) # they picked w so parse wal
             colors = hexToRgb(colors)
+            makeStyle(colors)
+            replaceSettings()
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Wal Steam 0.1.0') # create the flags from the comment
